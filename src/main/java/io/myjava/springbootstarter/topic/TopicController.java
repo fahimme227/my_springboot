f0db405a4bfb1b97119ee47058f1744ef288a18d
Fahim227 <islamfahim227@gmail.com>
2025-11-27 23:33:32 +0600

    feat: post,put and delete api added

--- src/main/java/io/myjava/springbootstarter/topic/TopicController.java
+++ src/main/java/io/myjava/springbootstarter/topic/TopicController.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +30,26 @@ public class TopicController {
 	public Topic getTopic(@PathVariable String id) {
 		return topicService.getTopic(id);
 	}
+
 	
+	
+	@PostMapping("/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		
+		topicService.addTopic(topic);
+	}
+	
+	
+	
+	@PutMapping("/topics/{id}")
+	public List<Topic> updateTopic(@RequestBody Topic topic,@PathVariable String id) {
+		return topicService.updateTopic(topic,id);
+	}
+	
+
+	@DeleteMapping("/topics/{id}")
+	public void deleteTopic(@PathVariable String id) {
+		topicService.deleteTopic(id);
+	}
 
 }
--- src/main/java/io/myjava/springbootstarter/topic/TopicService.java
+++ src/main/java/io/myjava/springbootstarter/topic/TopicService.java
@@ -1,5 +1,6 @@
 package io.myjava.springbootstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,11 +10,11 @@ import org.springframework.stereotype.Service;
 public class TopicService {
 	
 	
-	private List<Topic>  allTopics = Arrays.asList(
+	private List<Topic>  allTopics = new ArrayList<>(Arrays.asList(
 			new Topic("id1","JAva","description"),
 			new Topic("id2","JAva1.8","description1.8"),
 			new Topic("id3","JAva17","description17")
-	  );
+	  ));
 	
 	public List<Topic> getAllTopics() {
 		
@@ -26,5 +27,31 @@ public class TopicService {
 		return allTopics.stream().filter(e -> e.getId().equals(id)).findFirst().get();
 	}
 	
+	
+	
+	public void addTopic(Topic topic) {
+		allTopics.add(topic);
+		
+	}
+	
+	public List<Topic> updateTopic(Topic topic,String id) {
+		for (int index = 0; index < allTopics.size(); index++) 
+		{
+			 Topic filteredTopic = allTopics.get(index);
+			 if(filteredTopic.id.equals(id)) {
+				 allTopics.set(index, topic);
+				 
+			 }
+			
+		}
+		
+		return allTopics;
+		
+	}
+	
+	public void deleteTopic(String id) {
+		Topic filteredTopic = getTopic(id);
+		allTopics.remove(filteredTopic);
+	}
 
 }
